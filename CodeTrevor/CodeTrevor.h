b12e50a00e06645631c74af621a9119075e302df
Trevor Stevenson <trevor.stevenson@duke.edu>
2017-06-24 11:30:36 -0400

    Initial Commit

---
+++ CodeTrevor/CodeTrevor.h
@@ -0,0 +1,19 @@
+//
+//  CodeTrevor.h
+//  CodeTrevor
+//
+//  Created by Trevor Stevenson on 6/24/17.
+//  Copyright © 2017 Triple Torus Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CodeTrevor.
+FOUNDATION_EXPORT double CodeTrevorVersionNumber;
+
+//! Project version string for CodeTrevor.
+FOUNDATION_EXPORT const unsigned char CodeTrevorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CodeTrevor/PublicHeader.h>
+
+
