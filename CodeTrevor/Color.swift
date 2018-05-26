478415150cd5cf42dc106b67a77cd17623d1e308
Trevor Stevenson <trevor.stevenson@duke.edu>
2018-05-26 19:52:17 -0400

    Add Color extension

---
+++ CodeTrevor/Color.swift
@@ -0,0 +1,53 @@
+//
+//  Color.swift
+//  CodeTrevor
+//
+//  Created by Trevor Stevenson on 5/25/18.
+//  Copyright © 2018 Triple Torus Software. All rights reserved.
+//
+
+import Foundation
+
+public extension UIColor
+{
+    public convenience init(hex: String, alpha: CGFloat) {
+        
+        guard hex.count != 0 else {
+            self.init(red: 0.0, green: 0.0, blue: 0.0, alpha: 1.0)
+            return
+        }
+        
+        var hexString = hex
+        
+        if let first = hexString.first {
+            if first == "#" {
+                hexString.removeFirst()
+            }
+        }
+        
+        let hexValue = UInt32(hexString, radix: 16)
+        
+        if let hexVal = hexValue {
+            let r = (hexVal & 0x00FF0000) >> 16
+            let g = (hexVal & 0x0000FF00) >> 8
+            let b = hexVal & 0x000000FF
+
+            self.init(red: CGFloat(r)/255.0, green: CGFloat(g)/255.0, blue: CGFloat(b)/255.0, alpha: alpha)
+        }
+        else {
+            self.init(red: 0, green: 0, blue: 0, alpha: 1.0)
+        }
+    }
+    
+    public func darker(factor: CGFloat) -> UIColor
+    {
+        var red: CGFloat = 0
+        var green: CGFloat = 0
+        var blue: CGFloat = 0
+        var alpha: CGFloat = 0
+        
+        self.getRed(&red, green: &green, blue: &blue, alpha: &alpha)
+        
+        return UIColor(red: red * factor, green: green * factor, blue: blue * factor, alpha: 1.0)
+    }
+}
--- CodeTrevor/Pie.swift
+++ CodeTrevor/Pie.swift
@@ -35,13 +35,13 @@ public class Pie: UIView {
     override public func draw(_ rect: CGRect)
     {
         let shapeLayer = CAShapeLayer()
-        let colors: [UIColor] = [.red, .green, .gray]
+        let colors: [UIColor] = [UIColor(hex: "0984e3", alpha: 1.0), UIColor(hex: "6c5ce7", alpha: 1.0), UIColor(hex: "00b894", alpha: 1.0)]
         let cntr = CGPoint(x: frame.size.width/2, y: frame.size.height/2)
         let delta: CGFloat = 2
         
         var prevAngle = 3 * CGFloat.pi / 2
         
-        for i in 1...3 {
+        for i in 1...sections {
             let sublayer = CAShapeLayer()
             let path = UIBezierPath()
             path.move(to: CGPoint(point: cntr, offset: delta))
@@ -54,7 +54,7 @@ public class Pie: UIView {
             path.close()
             sublayer.path = path.cgPath
             sublayer.fillColor = colors[i % colors.count].cgColor
-            sublayer.strokeColor = UIColor.black.cgColor
+            sublayer.strokeColor = colors[i % colors.count].darker(factor: 0.25).cgColor
             sublayer.lineWidth = 2.0
             shapeLayer.addSublayer(sublayer)
         }
