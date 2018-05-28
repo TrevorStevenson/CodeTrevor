ccce0655e784708dcd937e895e4106ad42c411ea
Trevor Stevenson <trevor.stevenson@duke.edu>
2018-05-28 02:17:13 -0400

    Improve Pie chart look

--- CodeTrevor/Pie.swift
+++ CodeTrevor/Pie.swift
@@ -12,9 +12,11 @@ public class Pie: UIView {
     
     var sections: Int = 0
     var radius: CGFloat = 0
-    var delta: CGFloat = 5
-    var colors: [UIColor] = []
+    public var separation: CGFloat = 5
+    public var colors: [UIColor] = [.electronBlue, .brightYarrow, .chiGong, .exodusFruit, .orangeVille, .draculaOrchid, .prunusAvium, .robinsEggBlue]
     var values: [CGFloat] = []
+    public var labels: [String] = []
+    public var labelFont: UIFont = UIFont.systemFont(ofSize: 17)
     var totalValue: CGFloat = 0
     
     override public init(frame: CGRect)
@@ -22,16 +24,14 @@ public class Pie: UIView {
         super.init(frame: frame)
     }
     
-    public init(center: CGPoint, radius: CGFloat, separation: CGFloat = 5, values: [CGFloat],
-                colors: [UIColor] = [.electronBlue, .brightYarrow, .chiGong, .exodusFruit, .orangeVille, .draculaOrchid, .prunusAvium, .robinsEggBlue])
+    public init(center: CGPoint, radius: CGFloat, values: [CGFloat])
     {
-        super.init(frame: CGRect(origin: CGPoint(point: center, offset: -radius/2), size: CGSize(width: radius, height: radius)))
+        super.init(frame: CGRect(origin: CGPoint(point: center, offset:-radius-10), size: CGSize(width: 2*radius+20, height: 2*radius + CGFloat(values.count) * 30.0 + 15)))
+        
         self.sections = values.count
         self.radius = radius
         self.backgroundColor = .clear
         self.isOpaque = false
-        self.delta = separation
-        self.colors = colors
         self.values = values
         for value in values {
             self.totalValue += value
@@ -46,33 +46,44 @@ public class Pie: UIView {
     override public func draw(_ rect: CGRect)
     {
         let shapeLayer = CAShapeLayer()
-        let cntr = CGPoint(x: frame.size.width/2, y: frame.size.height/2)
+        let cntr = CGPoint(x: frame.size.width/2, y: 10+radius)
         var prevAngle = 3 * CGFloat.pi / 2
+        var currentY = 2*radius + 20
         
         for i in 0...sections-1 {
             let sublayer = CAShapeLayer()
             let path = UIBezierPath()
             let startAngle = prevAngle
             var endAngle = startAngle + (2 * CGFloat.pi * values[i] / totalValue)
-            let vertex = CGPoint(point: cntr, offsetX: delta * cos((startAngle + endAngle)/2), offsetY: delta * sin((startAngle + endAngle)/2))
-            
+            //let vertex = CGPoint(point: cntr, offsetX: separation * cos((startAngle + endAngle)/2), offsetY: separation * sin((startAngle + endAngle)/2))
             endAngle = endAngle.truncatingRemainder(dividingBy: 2 * CGFloat.pi)
-            let deltaAngle = (startAngle + endAngle) / 500
 
             prevAngle = endAngle
-            path.move(to: vertex)
-            path.addArc(withCenter: vertex, radius: radius, startAngle: startAngle+deltaAngle, endAngle: endAngle-deltaAngle, clockwise: true)
-            path.addLine(to: vertex)
+            path.move(to: cntr)
+            path.addArc(withCenter: cntr, radius: radius, startAngle: startAngle, endAngle: endAngle, clockwise: true)
+            path.addLine(to: cntr)
             path.close()
             sublayer.path = path.cgPath
             sublayer.fillColor = colors[i % colors.count].cgColor
             sublayer.strokeColor = colors[i % colors.count].darker(factor: 0.5).cgColor
-            sublayer.lineWidth = 4.0
+            sublayer.lineWidth = 2.0
+            
             shapeLayer.addSublayer(sublayer)
-            print(vertex)
+            let legendLayer = CAShapeLayer()
+            let legendSquare = UIBezierPath(roundedRect: CGRect(x: 20, y: currentY, width: 20, height: 20), cornerRadius: 5)
+            legendLayer.path = legendSquare.cgPath
+            legendLayer.fillColor = colors[i % colors.count].cgColor
+            legendLayer.strokeColor = colors[i % colors.count].darker(factor: 0.5).cgColor
+            legendLayer.lineWidth = 2.0
+            layer.addSublayer(legendLayer)
+            
+            let legendLabel = UILabel(frame: CGRect(x: 60, y: currentY, width: frame.size.width-60, height: 20))
+            legendLabel.text = labels[i]
+            legendLabel.textColor = .black
+            legendLabel.font = labelFont
+            addSubview(legendLabel)
+            currentY += 30
         }
-        
-        
         
         layer.addSublayer(shapeLayer)
     }
